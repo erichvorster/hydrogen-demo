1ade8213b95ecde486ecfbc7a3b7f792158f1270
erichvorster <erichvorster1@gmail.com>
2023-01-20 21:51:27 +0200

    initial commit

---
+++ hydrogen.config.ts
@@ -0,0 +1,28 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain:
+      // @ts-ignore
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'https://hydrogen-test-6154.myshopify.com/',
+    storefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
+      '6edc94c09d12e58ea8bd86d6b7c08570',
+    privateStorefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
+    storefrontApiVersion: '2022-07',
+    // @ts-ignore
+    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
